4d680693b4450c3458ae1b7a136a44f38aa12345
brucewp195-art <brucewp195@gmail.com>
2026-09-08 01:34:28 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+implementation("com.google.mlkit:text-recognition:16.0.1")
+
 }
