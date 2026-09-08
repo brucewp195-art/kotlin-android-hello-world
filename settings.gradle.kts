921e0eb233ee716a69b08d7afcf7b1686d7a794b
brucewp195-art <brucewp195@gmail.com>
2026-09-08 21:34:17 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        // This is the map that tells the build where to find Tesseract!
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
